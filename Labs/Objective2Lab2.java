754cdf385dfaaa8e0aedaa02bdc9e64cc507069b
Michael Ventura <m.ventura14@yahoo.com>
2024-01-14 14:35:12 -0600

    Completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+        String name = "Michael Ventura";
+        int age = 28;
+        String state = "Texas";
+
+        System.out.printf("Name: %s Age: %d State: %s %n", name,age,state);
+
+    }
+}
