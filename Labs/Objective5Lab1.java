77acdd63fd3af7a2f7d8d650b79fe3fcb77521c0
Michael Ventura <m.ventura14@yahoo.com>
2024-01-14 19:07:21 -0600

    Completed Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,13 @@
+public class Objective5Lab1 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 15;
+
+        if(num1 > num2) {
+            System.out.printf("%d is greater than $d %n", num1, num2);
+        }
+        else if(num2 > num1) {
+            System.out.printf("%d is greater than %d %n", num2, num1);
+        }
+    }
+}
