9dfd8ff7b508b22ea9e167409e956010e6c2c9ac
Michael Ventura <m.ventura14@yahoo.com>
2024-01-16 16:08:33 -0600

    Completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+    public static void main(String[] args) {
+        int count = 1;
+        int sum = 0;
+
+        while (count <= 20){
+//            System.out.println(count);
+            sum += count;
+//            System.out.println(sum);
+            count++;
+        }
+        System.out.println(sum);
+    }
+}
